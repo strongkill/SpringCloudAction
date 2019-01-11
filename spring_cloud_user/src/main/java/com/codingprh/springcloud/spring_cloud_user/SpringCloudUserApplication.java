aebad7ae357d0cdb9f9b1c42c13c17f141de57c0
PanRunHuan <Codingprh@gmail.com>
2019-01-11 17:52:38 +0800

    用户模块搭建

---
+++ spring_cloud_user/src/main/java/com/codingprh/springcloud/spring_cloud_user/SpringCloudUserApplication.java
@@ -0,0 +1,14 @@
+package com.codingprh.springcloud.spring_cloud_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+public class SpringCloudUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudUserApplication.class, args);
+    }
+
+}
+
